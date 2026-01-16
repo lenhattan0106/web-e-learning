81626169895d6beb5296761b6b82ad32a9fccfc3
lenhattan0106 <tanlienthuy1@gmail.com>
2026-01-16 19:58:21 +0700

    Capnhat Prisma Query Engine for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
-
+//@ts-ignore
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 const nextConfig: NextConfig = {
   images:{
     remotePatterns: [
@@ -24,7 +25,14 @@ const nextConfig: NextConfig = {
         protocol:'https'
       }
     ]
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 };
 
 export default nextConfig;
